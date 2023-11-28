4180fe826f21c9dc0cf9ca2f19a3664864dc787d
lucas <lucasluz1710@gmail.com>
2023-11-28 12:48:02 -0300

    created theme and install theme provider

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import theme from './styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+  <ThemeProvider theme={theme}>
+  </ThemeProvider>
 
   </React.StrictMode>,
 );
